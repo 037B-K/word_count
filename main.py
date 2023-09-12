e0e0fd9d6aafa057ca6af41816415fe344c4e5b5
bertrandkuniedal <bertrandkuniedal@educ.college-montreal.qc.ca>
2023-09-12 15:41:57 -0400

    rendus le trouch en fonction

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 # Compter le nombre de mots dans une phrchaine de mot inseree par l'utilisateur
 chaine = input(str("Inserer une chaine de mots: "))
-print("Nombre de mots: " + str(len(chaine.split())))
-# Utilisation du "str" pour ajouter(+) le resultat du nombre(len) de suite de lettre coupee par une space (split())
 
+
+def count_word():
+    mots = str(len(chaine.split()))
+    print("Il y a" + mots + "mots dans votre chaine")
+
+
+# Utilisation du "str" pour ajouter(+) le resultat du nombre(len) de suite de lettre coupee par une space (split())
+count_word()
