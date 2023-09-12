1a4ec56b8420988be67f5b037e38318fcbdbe06c
Léo Bertrand-Kunieda <bertrandkuniedal@educ.college-montreal.qc.ca>
2023-09-12 14:45:04 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Compter le nombre de mots dans une phrchaine de mot inseree par l'utilisateur
+chaine = input(str("Inserer une chaine de mots: "))
+print("Nombre de mots: " + str(len(chaine.split())))
+# Utilisation du "str" pour ajouter(+) le resultat du nombre(len) de suite de lettre coupee par une space (split())
+
